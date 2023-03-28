f9d2ab039bc04996ece1bcf6a352704e199bb15f
rgaynetdinov <foxcoolman@byndyusoft.com>
2023-03-28 16:16:38 +0500

    Добавил кейс без precision и recall

--- tests/Metrics.UnitTests/TestsData/Metrics/MultiClassClassificationMetricsCalculatorTestData.cs
+++ tests/Metrics.UnitTests/TestsData/Metrics/MultiClassClassificationMetricsCalculatorTestData.cs
@@ -32,6 +32,26 @@ namespace Byndyusoft.ML.Tools.Metrics.UnitTests.TestsData.Metrics
                     new ClassificationMetrics(0.619D, 0.591D, 0.605D),
                     new ClassificationMetrics(0.621D, 0.625D, 0.601D)),
                 Epsilon = 0.001D
+            },
+            new()
+            {
+                Description = "Cases without precision and recall",
+                Arguments = new ClassificationResult[]
+                {
+                    new("1", "1"),
+                    new("1", "2"),
+                    new("3", null)
+                },
+                ExpectedResult = new MultiClassClassificationMetrics(
+                    new OneClassClassificationMetrics[]
+                    {
+                        new("1", new ClassificationMetrics(1D, 0.5D, 0.667D)),
+                        new("2", new ClassificationMetrics(0D, null, 0D)),
+                        new("3", new ClassificationMetrics(null, 0D, 0D))
+                    },
+                    new ClassificationMetrics(0.5D, 0.333D, 0.4D),
+                    new ClassificationMetrics(0.5D, 0.25D, 0.222D)),
+                Epsilon = 0.001D
             }
         };
     }
